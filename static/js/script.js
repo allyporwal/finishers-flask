68f4cd2f3ce2dc0e24844ff8577e9b3df077bdf9
allyporwal <allyporwal@gmail.com>
2021-02-03 17:01:29 +0000

    Fix HTML validation errors and condense scripts.js

--- static/js/script.js
+++ static/js/script.js
@@ -17,6 +17,8 @@ fetch('/autofill')
     })
 
 
+// Load all other Materialize components and fade Flash messages out
+
 $(document).ready(function () {
     $('.sidenav').sidenav();
     $('select').formSelect();
@@ -27,116 +29,8 @@ $(document).ready(function () {
         });
 });
 
-// Dynamic field loading on create finisher function
+// Dynamic field loading on forms
 
-$("#exercise_set_type_select_1").change(function() {
-    $("#exercise_2").show("slow");
-});
-
-$("#exercise_set_type_select_2").change(function() {
-    $("#exercise_3").show("slow");
-});
-
-$("#exercise_set_type_select_3").change(function() {
-    $("#exercise_4").show("slow");
-});
-
-$("#exercise_set_type_select_4").change(function() {
-    $("#exercise_5").show("slow");
-});
-
-$("#exercise_set_type_select_5").change(function() {
-    $("#exercise_6").show("slow");
-});
-
-$("#exercise_set_type_select_6").change(function() {
-    $("#exercise_7").show("slow");
-});
-
-$("#exercise_set_type_select_7").change(function() {
-    $("#exercise_8").show("slow");
-});
-
-$("#exercise_set_type_select_8").change(function() {
-    $("#exercise_9").show("slow");
-});
-
-$("#exercise_set_type_select_9").change(function() {
-    $("#exercise_10").show("slow");
-});
-
-// Dynamic field loading on clone finisher page
-
-$("#edited_exercise_set_type_select_1").change(function() {
-    $("#edited_exercise_2").show("slow");
-});
-
-$("#edited_exercise_set_type_select_2").change(function() {
-    $("#edited_exercise_3").show("slow");
-});
-
-$("#edited_exercise_set_type_select_3").change(function() {
-    $("#edited_exercise_4").show("slow");
-});
-
-$("#edited_exercise_set_type_select_4").change(function() {
-    $("#edited_exercise_5").show("slow");
-});
-
-$("#edited_exercise_set_type_select_5").change(function() {
-    $("#edited_exercise_6").show("slow");
-});
-
-$("#edited_exercise_set_type_select_6").change(function() {
-    $("#edited_exercise_7").show("slow");
-});
-
-$("#edited_exercise_set_type_select_7").change(function() {
-    $("#edited_exercise_8").show("slow");
-});
-
-$("#edited_exercise_set_type_select_8").change(function() {
-    $("#edited_exercise_9").show("slow");
-});
-
-$("#edited_exercise_set_type_select_9").change(function() {
-    $("#edited_exercise_10").show("slow");
-});
-
-// Dynamic field loading on modify finisher page
-
-$("#modified_exercise_set_type_select_1").change(function() {
-    $("#modified_exercise_2").show("slow");
-});
-
-$("#modified_exercise_set_type_select_2").change(function() {
-    $("#modified_exercise_3").show("slow");
-});
-
-$("#modified_exercise_set_type_select_3").change(function() {
-    $("#modified_exercise_4").show("slow");
-});
-
-$("#modified_exercise_set_type_select_4").change(function() {
-    $("#modified_exercise_5").show("slow");
-});
-
-$("#modified_exercise_set_type_select_5").change(function() {
-    $("#modified_exercise_6").show("slow");
-});
-
-$("#modified_exercise_set_type_select_6").change(function() {
-    $("#modified_exercise_7").show("slow");
-});
-
-$("#modified_exercise_set_type_select_7").change(function() {
-    $("#modified_exercise_8").show("slow");
-});
-
-$("#modified_exercise_set_type_select_8").change(function() {
-    $("#modified_exercise_9").show("slow");
-});
-
-$("#modified_exercise_set_type_select_9").change(function() {
-    $("#modified_exercise_10").show("slow");
-});
+$('[id^=exercise_set_type_select_]').on('change', function() {
+    $(this).parent().parent().parent().next('.exercise-row').show('slow');
+})
